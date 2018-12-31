12f9414727ab7f08a2f72e7a0a77c79eb25c1ff9
FabioAJ <fabioaugusto.aj@gmail.com>
2018-12-31 11:21:10 -0200

    Padroniza o repositorio

---
+++ consultas.sql
@@ -0,0 +1,11 @@
+/*
+Nome do autor: Fábio Augusto Amaro Jaime
+Data de criação do arquivo: 31/12/2018
+Objetivo sucinto do arquivo: guardar resoluções de exercicios de banco de dados.
+Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r 
+*/
+
+
+
+ 
+
