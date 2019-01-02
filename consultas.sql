e554cd18933505da1b7f5450efbddc9b88c56d8d
FabioAJ <fabioaugusto.aj@gmail.com>
2019-01-02 16:40:01 -0200

    Faz um banco de dados e uma tabela

--- consultas.sql
+++ consultas.sql
@@ -1,11 +1,19 @@
 /*
-Nome do autor: Fábio Augusto Amaro Jaime
-Data de criação do arquivo: 31/12/2018
-Objetivo sucinto do arquivo: guardar resoluções de exercicios de banco de dados.
-Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r 
+Nome do autor: FÃ¡bio Augusto Amaro Jaime
+Data de criaÃ§Ã£o do arquivo: 31/12/2018
+Objetivo sucinto do arquivo: guardar resoluÃ§Ãµes de exercicios de banco de dados.
+ReferÃªncia: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r 
 */
 
+use cadastro;
+create table pessoas (
+nome varchar(30),
+idade tinyint(3),
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
 
 
- 
 
